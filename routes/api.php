37336f11f885b7bc539de37d844150e77a0dc2c2
fxreverLil <114184832+foreverLilRed@users.noreply.github.com>
2024-04-10 07:58:16 -0500

    Subida inicial

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+include_once 'v1/no-auth.php';
+
+Route::group(['middleware' => ['jwt.verify']], function () {
+    include_once 'v1/auth.php';
+});
